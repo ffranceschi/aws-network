a4d9dbbcad8962edf3286447092a60e566179130
Fernando Franceschi <fernando.franceschi@sumus.com.br>
2026-05-16 18:47:10 -0300

    feat: add hub nat gateway (single az, poc cost optimization)

---
+++ accounts/hub/nat.tf
@@ -0,0 +1,16 @@
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = { Name = "hub-nat-eip" }
+
+  depends_on = [module.vpc]
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = module.vpc.public_subnet_ids[0]
+
+  tags = { Name = "hub-nat-gw" }
+
+  depends_on = [module.vpc]
+}
