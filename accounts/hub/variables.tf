5f0a13a86de2edad607c95b3032110d616fe8693
Fernando Franceschi <fernando.franceschi@sumus.com.br>
2026-05-16 18:42:30 -0300

    feat: add hub account terraform providers and backend config

---
+++ accounts/hub/variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region for all resources"
+}
+
+variable "hub_account_id" {
+  type        = string
+  description = "AWS Account ID of the hub account"
+}
+
+variable "dev_account_id" {
+  type        = string
+  description = "AWS Account ID of the dev spoke account"
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner tag applied to all resources"
+}
+
+variable "state_bucket" {
+  type        = string
+  description = "S3 bucket name for Terraform remote state"
+}
+
+variable "lock_table" {
+  type        = string
+  description = "DynamoDB table name for Terraform state locking"
+}
+
+variable "dev_tgw_attachment_done" {
+  type        = bool
+  default     = false
+  description = "Set to true after accounts/dev is applied to add TGW route and association for dev spoke"
+}
